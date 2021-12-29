a8385ac0622d2bdb919703641d871a178e843ba5
Haklifesavr <58368333+Haklifesavr@users.noreply.github.com>
2021-12-29 22:37:02 +0500

    Add files via upload

---
+++ Variables&Input.py
@@ -0,0 +1,27 @@
+# Program for understanding variables and input function in python
+x = 1
+x = 2  # will override x = 1
+
+print(x)
+
+C_GPA: float = 2.99
+
+print(C_GPA)
+
+status: bool = True
+
+print(status)
+
+name = input("What is your name ? \n")
+
+color = input("What is your favorite color ? \n")
+
+print(name, " likes ", color)
+
+a = b = c = 1  # To assign multiple variables same values.
+
+print(a,b,c)
+
+i,j,k = 1,2.2,"haris"
+
+print(i,j,k)
